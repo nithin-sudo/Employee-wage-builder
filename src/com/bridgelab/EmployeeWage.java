63b8557ec80fa74f02e8f532db163580faa41305
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-22 19:30:07 +0530

    MODIFIED:EmployeeWage class which calculates employee sdaily wage

--- src/com/bridgelab/EmployeeWage.java
+++ src/com/bridgelab/EmployeeWage.java
@@ -3,14 +3,19 @@ package com.bridgelab;
 public class EmployeeWage {
     public static void main(String[] args){
         int IS_FULL_TIME=1;
+        int WAGE_PER_HOUR=20;
+        int FULL_WORK_HOUR=8;
+        int dailyWage;
         double random=Math.floor(Math.random()*10)%2;
         if (random==IS_FULL_TIME)
         {
             System.out.println("Employee is present");
+            dailyWage=(WAGE_PER_HOUR*FULL_WORK_HOUR);
+            System.out.println("Daily wage of an employee is:"+dailyWage);
         }
         else
         {
-            System.out.println("Employee is absent");
+            System.out.println("Employee is absent for the day his salary is zero");
         }
     }
 }
