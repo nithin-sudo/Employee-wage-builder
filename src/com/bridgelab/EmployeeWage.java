9afc0075c4c356a73a470700f87f8eb2aaa5fc98
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-22 19:36:21 +0530

    MODIFIED:EmployeeWage class and added switch case and calculated wage

--- src/com/bridgelab/EmployeeWage.java
+++ src/com/bridgelab/EmployeeWage.java
@@ -1,29 +1,27 @@
 package com.bridgelab;
 
 public class EmployeeWage {
+    static final int IS_FULL_TIME=1;
+    static final int IS_PART_PRESENT=2;
+    static int WAGE_PER_HOUR=20;
+    static int FULL_WORK_HOUR=8;
+    static int PART_WORK_HOUR=4;
     public static void main(String[] args){
-        int IS_FULL_TIME=1;
-        int IS_PART_PRESENT=2;
-        int WAGE_PER_HOUR=20;
-        int FULL_WORK_HOUR=8;
-        int PART_WORK_HOUR=4;
         int dailyWage;
         double empCheck=Math.floor(Math.random()*10)%3;
-        if (empCheck==IS_FULL_TIME)
-        {
-            System.out.println("Employee is full time present");
-            dailyWage=(WAGE_PER_HOUR*FULL_WORK_HOUR);
-            System.out.println("Daily wage of an employee is:"+dailyWage);
-        }
-        else if (empCheck==IS_PART_PRESENT)
-        {
-            System.out.println("employee is part time present");
-            dailyWage=(WAGE_PER_HOUR*PART_WORK_HOUR);
-            System.out.println("Daily wage of an employee is: :"+dailyWage);
-        }
-        else
-        {
-            System.out.println("Employee is absent for the day his salary is zero");
+        switch ((int)empCheck) {
+            case IS_FULL_TIME:
+                System.out.println("employee is present");
+                dailyWage=(WAGE_PER_HOUR*FULL_WORK_HOUR);
+                System.out.println("salary is:"+dailyWage);
+                break;
+            case IS_PART_PRESENT:
+                dailyWage=(WAGE_PER_HOUR*PART_WORK_HOUR);
+                System.out.println("salary is :"+dailyWage);
+                System.out.println("employee is part time present");
+                break;
+            default:
+                System.out.println("when employee is absent for the day his salary is zero");
         }
     }
 }
